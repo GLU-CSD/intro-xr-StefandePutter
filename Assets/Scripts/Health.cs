06b1a70e6367f99118ef5dde8ca6a2fde841cc6c
StefandePutter <Stefandeputter@gmail.com>
2025-01-06 12:47:45 +0100

    finished 1 to 6

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+    public Image healthbarFill;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    public void RestoreHealth(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthbarFill.fillAmount = currentHealth/maxHealth;
+    }
+}
